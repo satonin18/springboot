36e41ee6df7ee47ec532c1e17fa932d427b08238
satonin18 <satonin18@mail.ru>
2019-09-17 18:41:00 +0500

    use inner-db

--- src/main/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/SpringbootApplication.java
+++ src/main/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/SpringbootApplication.java
@@ -2,7 +2,11 @@ package com.lanit.dcs.diss.aacs.satonin18.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.lanit.dcs.diss.aacs.satonin18.hackathon.web.repository")
+@EntityScan("com.lanit.dcs.diss.aacs.satonin18.hackathon.web.entity")
 @SpringBootApplication
 public class SpringbootApplication {
 
