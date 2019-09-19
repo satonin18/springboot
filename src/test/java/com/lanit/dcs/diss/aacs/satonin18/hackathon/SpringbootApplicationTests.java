309d3e1bc616e640d3d05ea596ddb34e53e3f1b3
satonin18 <satonin18@mail.ru>
2019-09-19 13:01:11 +0500

    10 test

--- src/test/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/SpringbootApplicationTests.java
+++ src/test/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/SpringbootApplicationTests.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.assertEquals;
 //todo НАЗВАНИЯ ПЕРЕМЕННЫХ НЕ СООТВЕСТВУЮТ КОД_КОНВЕНШН, ТАК КАК НЕКОТОРЫЕ МЕТОДЫ(например get) асертят (проверяют валидность)
 // НО ЗАТО НАЗВАНИЯ МЕТОДОВ СООТВЕСТВУЮТ МЕТОДАМ SOAP UI
 
+// todo ПОНЯТЬ КАК РАБОТАЮТ ЭТИ АНОТАЦИИ //ПРОВЕРКА ДОЛЖНА БЫТЬ ВНУТРЕНЯЯЯ ИЛИ ВНЕШНЯЯ (КАК В SoapUI)//здесь какая???
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { SpringbootApplication.class },
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -26,22 +28,22 @@ public class SpringbootApplicationTests {
         System.out.println("----testClear----");
 
         http.clear();
-        check_addperson();
+        test_add_person();
         http.clear();
-        check_get();
+        test_get();
 
         System.out.println("----------------");
     }
-    private void check_addperson() {
-        System.out.println("->check_addperson()");
+    private void test_add_person() {
+        System.out.println("->test_add_person()");
 
-        Response r = http.addPerson(jsonMap.get("addperson").getJson());
+        Response r = http.addPerson(jsonMap.get("add_person").getJson());
         assertEquals(HttpStatus.OK.value(), r.getStatusCode());
 
         System.out.println("<-");
     }
-    private void check_get() {
-        System.out.println("->check_get()");
+    private void test_get() {
+        System.out.println("->test_get()");
 
         Response r = http.get(jsonMap.get("get").getId());
         assertEquals(HttpStatus.NOT_FOUND.value(), r.getStatusCode());
@@ -54,27 +56,231 @@ public class SpringbootApplicationTests {
         System.out.println();
         System.out.println("----testAddValidPerson----");
 
-        check_addvalid1();
-        check_get1();
+        test_add_valid1();
+        test_get1();
 
         System.out.println("----------------");
     }
-    private void check_addvalid1() {
-        System.out.println("->check_addvalid1()");
+    private void test_add_valid1() {
+        System.out.println("->test_add_valid1()");
 
-        Response r = http.addPerson(jsonMap.get("addvalid1").getJson());
+        Response r = http.addPerson(jsonMap.get("add_valid1").getJson());
         assertEquals(HttpStatus.OK.value(), r.getStatusCode());
 
         System.out.println("<-");
     }
-    private void check_get1() {
-        System.out.println("->check_get()");
+    private void test_get1() {
+        System.out.println("->test_get1()");
 
         Response r = http.get(jsonMap.get("get1").getId());
         assertEquals(HttpStatus.OK.value(), r.getStatusCode());
         //todo check vars -> jsonParsing->everyVar==requestVar
         //simple approach
         assertEquals(jsonMap.get("get1").getJson(), r.getBody().print());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddValidPersonLess18() {
+        System.out.println();
+        System.out.println("----testAddValidPersonLess18----");
+
+        test_add_valid2();
+        test_get2();
+
+        System.out.println("----------------");
+    }
+    private void test_add_valid2() {
+        System.out.println("->check_add_valid2()");
+
+        Response r = http.addPerson(jsonMap.get("add_valid2").getJson());
+        assertEquals(HttpStatus.OK.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_get2() {
+        System.out.println("->test_get2()");
+
+        Response r = http.get(jsonMap.get("get2").getId());
+        assertEquals(HttpStatus.OK.value(), r.getStatusCode());
+        //todo check vars -> jsonParsing->everyVar==requestVar
+        //simple approach
+        assertEquals(jsonMap.get("get2").getJson(), r.getBody().print());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddValidPersonEmptyName() {
+        System.out.println();
+        System.out.println("----testAddValidPersonEmptyName----");
+
+        test_add_valid3_emptyname();
+        test_get3();
+
+        System.out.println("----------------");
+    }
+    private void test_add_valid3_emptyname() {
+        System.out.println("->check_add_valid3_emptyname()");
+
+        Response r = http.addPerson(jsonMap.get("add_valid3_emptyname").getJson());
+        assertEquals(HttpStatus.OK.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_get3() {
+        System.out.println("->test_get3()");
+
+        Response r = http.get(jsonMap.get("get3").getId());
+        assertEquals(HttpStatus.OK.value(), r.getStatusCode());
+        //todo check var name = "";
+        //simple approach
+        assertEquals(jsonMap.get("get3").getJson(), r.getBody().print());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonFutureBirthdate() {
+        System.out.println();
+        System.out.println("----testAddValidFutureBirthdate----");
+
+        test_add_notvalid4_futurebirthdate();
+        test_getnot4();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid4_futurebirthdate() {
+        System.out.println("->check_add_valid4_futurebirthdate()");
+
+        Response r = http.addPerson(jsonMap.get("add_valid4_futurebirthdate").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_getnot4() {
+        System.out.println("->test_getnot4()");
+
+        Response r = http.get(jsonMap.get("getnot4").getId());
+        assertEquals(HttpStatus.NOT_FOUND.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonEmptyId() {
+        System.out.println();
+        System.out.println("----testAddBadPersonEmptyId----");
+
+        test_add_notvalid_emptyid();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid_emptyid() {
+        System.out.println("->test_add_notvalid_emptyid()");
+
+        Response r = http.addPerson(jsonMap.get("add_notvalid_emptyid").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonNullId() {
+        System.out.println();
+        System.out.println("----testAddBadPersonNullId----");
+
+        test_add_notvalid_nullid();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid_nullid() {
+        System.out.println("->test_add_notvalid_nullid()");
+
+        Response r = http.addPerson(jsonMap.get("add_notvalid_nullid").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonNullName() {
+        System.out.println();
+        System.out.println("----testAddBadPersonNullName----");
+
+        test_add_notvalid5_nullname();
+        test_getnot5();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid5_nullname() {
+        System.out.println("->test_add_notvalid5_nullname()");
+
+        Response r = http.addPerson(jsonMap.get("add_notvalid5_nullname").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_getnot5() {
+        System.out.println("->test_getnot5()");
+
+        Response r = http.get(jsonMap.get("getnot5").getId());
+        assertEquals(HttpStatus.NOT_FOUND.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonNullBirthdate() {
+        System.out.println();
+        System.out.println("----testAddBadPersonNullBirthdate----");
+
+        test_add_notvalid6_nullbirthdate();
+        test_getnot6();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid6_nullbirthdate() {
+        System.out.println("->test_add_notvalid6_nullbirthdate()");
+
+        Response r = http.addPerson(jsonMap.get("add_notvalid6_nullbirthdate").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_getnot6() {
+        System.out.println("->test_getnot6()");
+
+        Response r = http.get(jsonMap.get("getnot6").getId());
+        assertEquals(HttpStatus.NOT_FOUND.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+
+    @Test
+    public void testAddBadPersonIncorrectBirthdate() {
+        System.out.println();
+        System.out.println("----testAddBadPersonNullBirthdate----");
+
+        test_add_notvalid7_incorrectbirthdate();
+        test_getnot7();
+
+        System.out.println("----------------");
+    }
+    private void test_add_notvalid7_incorrectbirthdate() {
+        System.out.println("->test_add_notvalid7_incorrectbirthdate()");
+
+        Response r = http.addPerson(jsonMap.get("add_notvalid7_incorrectbirthdate").getJson());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), r.getStatusCode());
+
+        System.out.println("<-");
+    }
+    private void test_getnot7() {
+        System.out.println("->test_getnot7()");
+
+        Response r = http.get(jsonMap.get("getnot7").getId());
+        assertEquals(HttpStatus.NOT_FOUND.value(), r.getStatusCode());
 
         System.out.println("<-");
     }
--- src/test/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/Templates.java
+++ src/test/java/com/lanit/dcs/diss/aacs/satonin18/hackathon/Templates.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @Data
-@AllArgsConstructor //ATTATION  //no EDIT !!!
+@AllArgsConstructor //ATTATION //no EDIT !!!
 class Struct{
     private int id;
     private String json;
@@ -25,7 +25,7 @@ public class Templates {
     public static final Map<String, Struct> jsonMap = new HashMap<>();
 
     static {
-        //TODO generetor ID (then INCLUDE IN JSON by stringConcantining)
+        //TODO INCLUDE ID IN JSON by stringConcantining)
         int id = 0;
         String json4save = Strings.EMPTY;
         String json4output = Strings.EMPTY;
@@ -33,13 +33,57 @@ public class Templates {
         id = -1;
         json4save = "{\"id\":\"-1\",\"name\":\"Validperson1\",\"birthdate\":\"01.01.2000\"}";
         json4output = Strings.EMPTY;
-        jsonMap.put("addperson", new Struct(id, json4save));
+        jsonMap.put("add_person", new Struct(id, json4save));
         jsonMap.put("get", new Struct(id, json4output));
 
         id = -10;
-        json4save = "{\"id\":-10,\"name\":\"Validperson1\",\"birthdate\":\"01.01.2000\",\"cars\":[]}\"";
+        json4save = "{\"id\":-10,\"name\":\"Validperson1\",\"birthdate\":\"01.01.2000\",\"cars\":[]}";
         json4output = "{\"id\":-10,\"name\":\"Validperson1\",\"birthdate\":\"01.01.2000\",\"cars\":[]}";
-        jsonMap.put("addvalid1", new Struct(id, json4save));
+        jsonMap.put("add_valid1", new Struct(id, json4save));
         jsonMap.put("get1", new Struct(id, json4output));
+
+        id = -20;
+        json4save = "{\"id\":\"-20\",\"name\":\"Validperson2\",\"birthdate\":\"01.12.2017\"}";
+        json4output = "{\"id\":-20,\"name\":\"Validperson2\",\"birthdate\":\"01.12.2017\",\"cars\":[]}";
+        jsonMap.put("add_valid2", new Struct(id, json4save));
+        jsonMap.put("get2", new Struct(id, json4output));
+
+        id = -30;
+        json4save = "{\"id\":\"-30\",\"name\":\"\",\"birthdate\":\"01.12.2017\"}";
+        json4output = "{\"id\":-30,\"name\":\"\",\"birthdate\":\"01.12.2017\",\"cars\":[]}";
+        jsonMap.put("add_valid3_emptyname", new Struct(id, json4save));
+        jsonMap.put("get3", new Struct(id, json4output));
+
+        id = -39;
+        json4save = "{\"id\":\"-39\",\"name\":\"valid\",\"birthdate\":\"01.12.2117\"}";
+        json4output = Strings.EMPTY;
+        jsonMap.put("add_valid4_futurebirthdate", new Struct(id, json4save));
+        jsonMap.put("getnot4", new Struct(id, json4output));
+
+        id = 0; //todo no corect
+        json4save = "{\"id\":\"\",\"name\":\"valid\",\"birthdate\":\"01.12.2017\"}";
+        jsonMap.put("add_notvalid_emptyid", new Struct(id, json4save));
+
+        id = 0; //todo no corect
+        json4save = "{\"name\":\"valid\",\"birthdate\":\"01.12.2017\"}";
+        jsonMap.put("add_notvalid_nullid", new Struct(id, json4save));
+
+        id = -70;
+        json4save = "{\"id\":\"-70\",\"birthdate\":\"01.12.2017\"}";
+        json4output = Strings.EMPTY;
+        jsonMap.put("add_notvalid5_nullname", new Struct(id, json4save));
+        jsonMap.put("getnot5", new Struct(id, json4output));
+
+        id = -80;
+        json4save = "{\"id\":\"-80\",\"name\":\"valid\"}";
+        json4output = Strings.EMPTY;
+        jsonMap.put("add_notvalid6_nullbirthdate", new Struct(id, json4save));
+        jsonMap.put("getnot6", new Struct(id, json4output));
+
+        id = -90;
+        json4save = "{\"id\":\"-90\",\"name\":\"Validperson2\",\"birthdate\":\"2017-01-01\"}";
+        json4output = Strings.EMPTY;
+        jsonMap.put("add_notvalid7_incorrectbirthdate", new Struct(id, json4save));
+        jsonMap.put("getnot7", new Struct(id, json4output));
     }
 }
